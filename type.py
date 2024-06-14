23fb393c74e4663e5acb7fdde6dbc70c83841d13
Nicolas Galha <nclsgalha@gmail.com>
2024-06-13 21:04:06 -0300

    criando arquivo de type

--- type.py
+++ type.py
@@ -1,9 +1,11 @@
 nome = "Paula Martins "
-Mes= "Janeiro"
-Valor = 500
-Desconto = 10
-Cupom = "PAULAÉ10"
+mes= "Janeiro"
+valor = 500
+desconto = 10
+cupom = "PAULAÉ10"
 
 tipo_nome = type(nome)
+tipo_desconto = type(desconto)
 
 print(tipo_nome)
+print(tipo_desconto)
